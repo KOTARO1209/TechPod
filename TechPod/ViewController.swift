4df5c418ccb66303fac7787358df74aec7d2ba93
KOTARO1209 <kmachida@mxdlab.net>
2023-02-06 23:59:28 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by まちだこうたろう on 2023/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
